8988ec245b8fd1f6b4127955165973c18e6768b9
Kenny Yepez <kfyepez@espol.edu.ec>
2021-08-07 21:42:46 -0500

    Limpieza 2

--- dawn/dawn/citas/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from citas.models import *
-
-# Register your models here.
-class NegocioAdmin(admin.ModelAdmin):
-    list_display= ("nombre","ruc","categoria","descripcion",)
-
-admin.site.register(Negocio,NegocioAdmin)
